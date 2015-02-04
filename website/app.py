76d2fab7f23d9ec4841949cd90a2cb5b7dee3eed
Xiaochen <weixc123@gmail.com>
2015-02-04 18:11:22 -0500

    初始化part3的网站架构
    完成homepage, login, index页，基本CSS配色与设置

---
+++ website/app.py
@@ -0,0 +1,28 @@
+__author__ = 'wei'
+
+from flask import Flask, jsonify, render_template, request
+import requests
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        # check if log in successfully
+        return render_template("homepage.html", data = str((username, password)))
+    else:
+        return render_template("login.html")
+
+
+
+
+if __name__ == "__main__":
+    app.run(host = "0.0.0.0")
