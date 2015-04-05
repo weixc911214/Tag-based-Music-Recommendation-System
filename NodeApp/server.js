eb7f3666c4679e34837fe7c6e11b96b040dcbf24
aiyyoi <amyxjwang91@gmail.com>
2015-04-05 01:34:32 -0400

    node modules

---
+++ NodeApp/server.js
@@ -0,0 +1,112 @@
+//Setup web server and socket
+var express = require('express'),
+    mysql = require('mysql'),
+    bodyParser = require('body-parser'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app);
+
+var musicdb = new mysql.createConnection({
+  host     : 'cs4111.cnjryr7wwjlv.us-west-2.rds.amazonaws.com',
+  user     : 'xw2355xw2353',
+  password : 'musicrecommendation',
+  database : 'cs4111'
+});
+//connect to specified database
+musicdb.connect(function(err){
+  if(!err) {
+      console.log("Database is connected ...");  
+  } else {
+      console.log("Error connecting database ...");  
+  }
+});
+
+//Use the default port (for beanstalk) or default to 8081 locally
+server.listen(process.env.PORT || 8081);
+
+//Setup rotuing for app
+//app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/html'));
+app.use(bodyParser());
+
+//app routing methods
+app.get('/',function(req,res){
+  res.sendFile('index.html');
+  //It will find and locate index.html from View or Scripts
+});
+
+app.get('/tosignup', function (req, res){
+	res.sendfile('signup.html', {root: './public/html/'});
+})
+
+app.post('/login', function (req, res){
+  //check passwords
+  var name = req.body.username;
+  var pwd = req.body.password;
+  var user_col = ['user_name', 'user_pwd'];
+	    var query = musicdb.query('select ?? from ?? where user_name = ?', [user_col, 'users', name], 
+	      function (err, rows, fields){
+	        if(err)
+	          console.log(err);
+	        else if(!rows.length){
+	          console.log("User does not exist");
+	          res.sendfile('index.html', {root: './public/html/'});
+	        }
+	        else if(rows[0].user_pwd !== pwd){
+	          console.log("Wrong password");
+	          res.sendfile('index.html', {root: './public/html/'});
+	        }
+	        else 
+	          // if everything is OK, return null as the error
+	          // and the authenticated user
+	          console.log("Login Success")
+	          res.sendfile('homepage.html', {root: './public/html/'});
+	    });
+	    console.log(query.sql);
+
+});
+ 
+app.post('/signup', function (req, res){
+  var name = req.body.username;
+  var pwd = req.body.password;
+  var conf = req.body.confirm;
+  var query = musicdb.query('select ?? from ?? where user_name = ?', ['user_name', 'users', name], 
+      function (err, rows, fields){
+        if(err)
+          console.log(err);
+        else if(rows.length){
+          console.log("The username is taken");
+          res.sendFile('signup.html', {root: './public/html/'});
+        }
+        else if(pwd !== conf){
+        		console.log("The username is taken");
+          	res.sendFile('signup.html', {root: './public/html/'});
+        }
+        else{
+          // if the name does not exist, write to database
+          musicdb.query('select max(user_id) as mIndex from users', function (err, rows, fields){
+            if(err)
+              console.log(err);
+            else if(!rows.length){
+              console.log("Unexpected error in database");
+              res.sendFile('signup.html', {root: './public/html/'});
+            }
+            else{
+              var user_details = {"user_id": rows[0].mIndex+1, "user_name": name, "user_pwd": pwd};
+              musicdb.query('INSERT INTO users SET ?', user_details,
+                function (err, result){
+                  if(err)
+                    console.log(err);
+              });
+              res.sendFile('homepage.html', {root: './public/html/'});
+            }
+          });
+        }
+    });
+    console.log(query.sql);
+});
+
+
+
+
+
